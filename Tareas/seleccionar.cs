0f77fde5e955f9d3093d076d2d5abe3a1360b4a8
Gerardo Aguilar <gerardo.aguilar@thinkingpeople.es>
2020-12-30 10:20:53 +0100

    ejer1

---
+++ Tareas/seleccionar.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Tareas
+{
+    class seleccionar
+    {
+        public void SeleccionarEjercicio()
+        {
+            Console.WriteLine("Selecciona el ejercicio que deseeas");
+            string n = Console.ReadLine();
+            int Num_Ejer = Convert.ToInt32(n);
+
+            switch (Num_Ejer)
+            {
+                case 1:
+                    ejercicio1 ejer = new ejercicio1();
+                    ejer.ElegirFigura();
+                    break;
+                default:
+
+                    break;
+            }
+        }
+    }
+}
